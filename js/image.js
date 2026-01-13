3bc4d67fe2078e0985045ec4588c7515385b43e2
xeuthestoic <136644443+xeuthestoic@users.noreply.github.com>
2026-01-13 11:12:21 +0000

    Update image.js

--- js/image.js
+++ js/image.js
@@ -1,1 +1,15 @@
-
+async function compressImage(file){
+  return new Promise(res => {
+    const img = new Image();
+    img.onload = () => {
+      const c = document.createElement("canvas");
+      const scale = 400 / img.width;
+      c.width = 400;
+      c.height = img.height * scale;
+      const ctx = c.getContext("2d");
+      ctx.drawImage(img,0,0,c.width,c.height);
+      res(c.toDataURL("image/webp", 0.7));
+    };
+    img.src = URL.createObjectURL(file);
+  });
+}
