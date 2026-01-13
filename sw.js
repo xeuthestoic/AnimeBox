51f21695d9f1dada2bc3e2c9b101daac7baeb9f5
xeuthestoic <136644443+xeuthestoic@users.noreply.github.com>
2026-01-13 11:13:31 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.open("animebox").then(cache =>
+      cache.match(e.request).then(res =>
+        res || fetch(e.request).then(net => {
+          cache.put(e.request, net.clone());
+          return net;
+        })
+      )
+    )
+  );
+});
